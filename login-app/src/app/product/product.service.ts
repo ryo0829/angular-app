d05e31b0f4768b1456fc464b7ad4af6fb7f985e0
ryo0829 <volleyball.ryo0829@gmail.com>
2020-02-25 22:44:06 +0900

    警告文削除

--- login-app/src/app/product/product.service.ts
+++ login-app/src/app/product/product.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
 
-import { Product } from './Product';
+import { Product } from './product';
 
-//@Injectableデコレーターを付与
+// @Injectableデコレーターを付与
 @Injectable()
 export class ProductService {
-  //商品情報を配列として返す
+  // 商品情報を配列として返す
   getProduct(): Product[] {
     return [
-      {No:1,Name:"鮭の切り身",Price:200,Stock:-1,Date: new Date(2020, 10, 1)},
-      {No:2,Name:"もも肉",Price:1000,Stock:50,Date: new Date(2019, 10, 1)},
-      {No:3,Name:"納豆　5パック",Price:100,Stock:120,Date: new Date(2020, 5, 21)},
-      {No:4,Name:"ふりかけ",Price:100,Stock:2000,Date: new Date(2020, 3, 1)},
-      {No:5,Name:"キャベツ",Price:200,Stock:10,Date: new Date(2017, 10, 1)},
+      {No: 1, Name: '鮭の切り身', Price: 200, Stock: -1, Date: new Date(2020, 10, 1)},
+      {No: 2, Name: 'もも肉', Price: 1000, Stock: 50, Date: new Date(2019, 10, 1)},
+      {No: 3, Name: '納豆　5パック', Price: 100, Stock: 120, Date: new Date(2020, 5, 21)},
+      {No: 4, Name: 'ふりかけ', Price: 100, Stock: 2000, Date: new Date(2020, 3, 1)},
+      {No: 5, Name: 'キャベツ', Price: 200, Stock: 10, Date: new Date(2017, 10, 1)},
     ];
   }
 }
